9a36c8babfe2231ee9fcead0000ab25e9046fbf8
Aditya Sharma <114939325+aditya2003sharma@users.noreply.github.com>
2024-10-11 15:22:54 +0000

    new

---
+++ 01_Basics/datentime.js
@@ -0,0 +1,12 @@
+let myDate=new Date()
+
+console.log(myDate.toString())
+console.log(myDate.toISOString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+
+let mytimeStamp=Date.now()
+console.log()//to convert in seconds divide by 1000
+
+// getmonth
+// getday
