b09183db9ee31e6374569464459d7867b4c5ca5c
Aditya Sharma <114939325+aditya2003sharma@users.noreply.github.com>
2024-10-11 15:05:45 +0000

    new

---
+++ 01_Basics/numbers_maths.js
@@ -0,0 +1,33 @@
+const balance =new Number(100)
+
+console.log(balance.toFixed(2))
+const balance2 =new Number(123.44)
+
+
+console.log(balance2.toPrecision(2))
+
+
+// console.log(Math)
+// console.log(Math); 
+// console.log(Math.abs(-4)); 
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9)); 
+// console.log(Math.min(4, 3, 6, 8)); 
+// console.log(Math.max(4, 3, 6, 8));
+
+
+
+console.log(Math.random())
+
+
+// const max=20
+// const min=10
+
+
+// console.log(Math.floor(Math.random()*(max-min+1)+min))
+
+
+
+
+
