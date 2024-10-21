4b42fc2f798a9e95a4ae86ab9ceb147f5bc7a54a
Aditya Sharma <114939325+aditya2003sharma@users.noreply.github.com>
2024-10-21 08:40:58 +0000

    events

---
+++ Events/1.js
@@ -0,0 +1,20 @@
+/*
+Synchronous language
+Single threaded language
+
+Each operation waits for the last one for execution
+
+Blocking code vs non blocking code
+
+Blocking code:
+Block the flow  of program
+Read file sync
+
+
+Non Blocking code
+Do not blocking execution
+Read file async
+
+
+
+*/
